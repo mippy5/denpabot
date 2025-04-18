b5372d48b294235415108e51bbe44483adcb9843
mippy5 <mip5@protonmail.com>
2025-04-18 00:27:58 -0400

    filter words but ignore actual matchers in the word list, also dm the deleted message to the author

---
+++ copy.sh
@@ -0,0 +1,1 @@
+scp target/x86_64-unknown-linux-gnu/release/denpabot root@23.184.48.171:~
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::builder::CreateMessage;
 use serenity::prelude::*;
 use std::collections::HashMap;
 use std::path::Path;
 use std::sync::RwLock;
+use std::time::Instant;
 use serde::{Deserialize, Serialize};
 
 const ENV_PATHS: &[&'static str] = &[
@@ -13,6 +15,8 @@ const ENV_PATHS: &[&'static str] = &[
 ];
 
 const APP_DATA_FILE: &str = "appdata.bin";
+
+const APP_WORD_LIST: &str = include_str!("../wordlist.txt");
 
 #[derive(Debug, Default)]
 struct TrieNode {
@@ -26,6 +30,10 @@ struct Trie {
 }
 
 impl Trie {
+	fn reset(&mut self) {
+		self.root = TrieNode::default();
+	}
+
 	fn insert(&mut self, word: &str) {
 		if word.len() == 0 {
 			return;
@@ -40,21 +48,24 @@ impl Trie {
 		node.end = true;
 	}
 
-	fn find_match(&self, input: &str) -> bool {
+	fn find_matches(&self, input: &str) -> Vec<(usize, usize)> {
+		let mut matches = Vec::<(usize, usize)>::default();
 		let mut cursor_it = input.chars();
 		let mut local_it = cursor_it.clone();
-		for _ in 0..input.len() {
+		for start in 0..input.len() {
 			// traverse the tree with the local iterator
 			let mut node= &self.root;
+			let mut end = 0;
 			loop {
-				if node.end {
-					return true;
-				}
 				match local_it.next() {
 					Some(c) => {
 						let v = node.children.get(&c);
 						if v.is_some() {
 							node = v.unwrap();
+							if node.end {
+								matches.push((start, start + end));
+							}
+							end += 1;
 						}
 						else {
 							break
@@ -67,6 +78,21 @@ impl Trie {
 			}
 			cursor_it.next();
 			local_it = cursor_it.clone();
+		}
+		matches
+	}
+
+	fn find_word(&self, input: &str) -> bool {
+		let mut node = &self.root;
+		for c in input.chars() {
+			if let Some(pair) = node.children.get_key_value(&c) {
+				node = pair.1;
+				if node.end {
+					return true;
+				}
+				continue
+			}
+			return false;
 		}
 		false
 	}
@@ -82,7 +108,7 @@ impl AppData {
 	fn build_trie(&self) -> Trie {
 		let mut trie = Trie::default();
 		for word in self.words.iter() {
-			trie.insert(word.as_str());
+			trie.insert(word.to_lowercase().as_str());
 		}
 		trie
 	}
@@ -91,6 +117,7 @@ impl AppData {
 #[derive(Default)]
 struct Handler {
 	censor_list: RwLock<Trie>,
+	allow_list: RwLock<Trie>,
 	app_data: RwLock<AppData>,
 }
 
@@ -153,9 +180,29 @@ impl EventHandler for Handler {
 				return;
 			}
 		}
-		if self.censor_list.read().unwrap().find_match(msg.content.as_str()) {
-			if let Err(why) = msg.delete(&ctx.http).await {
-				println!("Error deleting message: {why:?}");
+		let content = msg.content.to_lowercase();
+		let censors = self.censor_list.read().unwrap().find_matches(content.as_str());
+		if !censors.is_empty() {
+			let allows = self.allow_list.read().unwrap().find_matches(content.as_str());
+			// check the censors to see if there's an allow around it
+			let mut checks = censors.len();
+			for censor in censors.iter() {
+				for allow in allows.iter() {
+					if allow.0 <= censor.0 && allow.1 >= censor.1 {
+						checks -= 1;
+						break;
+					}
+				}
+			}
+			if checks > 0 {
+				if let Err(why) = msg.delete(&ctx.http).await {
+					println!("Error deleting message: {why:?}");
+				}
+				let mut dm_msg = CreateMessage::default();
+				dm_msg = dm_msg.content(format!("Your message was deleted:\n```\n{}\n```", msg.content));
+				if let Err(why) = msg.author.dm(&ctx.http, dm_msg).await {
+					println!("Error deleting message: {why:?}");
+				}
 			}
 		}
 	}
@@ -172,11 +219,12 @@ impl EventHandler for Handler {
 
 impl Handler {
 	fn save(&self) {
-		let app_data_guard = self.app_data.read().unwrap();
-		let app_data = serde_cbor::to_vec(&*app_data_guard).unwrap();
-		std::fs::write(APP_DATA_FILE, app_data).unwrap();
-		// rebuild the censor list
-		*self.censor_list.write().unwrap() = app_data_guard.build_trie();
+		{
+			let app_data_guard = self.app_data.read().unwrap();
+			let app_data = serde_cbor::to_vec(&*app_data_guard).unwrap();
+			std::fs::write(APP_DATA_FILE, app_data).unwrap();
+		}
+		self.build();
 	}
 
 	fn load(&mut self) {
@@ -184,12 +232,32 @@ impl Handler {
 			Ok(data) => {
 				let mut app_data = self.app_data.write().unwrap();
 				*app_data = serde_cbor::from_slice(&data[..]).unwrap();
-				*self.censor_list.write().unwrap() = app_data.build_trie();
 			}
 			Err(_) => {
 				println!("Failed to load list.dat")
 			}
 		}
+		self.build();
+	}
+
+	fn build(&self) {
+		let timer = Instant::now();
+		// rebuild the censor list
+		let app_data_guard = self.app_data.read().unwrap();
+		*self.censor_list.write().unwrap() = app_data_guard.build_trie();
+		{
+			let cl = self.censor_list.read().unwrap();
+			let mut al = self.allow_list.write().unwrap();
+			al.reset();
+			for word in APP_WORD_LIST.lines() {
+				if cl.find_word(word) {
+					continue;
+				}
+				al.insert(word);
+			}
+		}
+		let build_time = Instant::now() - timer;
+		println!("It took {} seconds to build the lists", build_time.as_secs_f32());
 	}
 
 	async fn say_list(&self, ctx: &Context, msg: &Message, on_update: bool) {
